d012e4577dd2fe656d955a02745cf137cd0f98c0
rhoth <94731244+rhoth@users.noreply.github.com>
2021-12-19 00:18:34 +0530

    stage4

--- quiz1.js
+++ quiz1.js
@@ -1,238 +1,119 @@
 const questions = [
     {
-        question: "How many days makes a week ?",
-        optionA: "10 days",
-        optionB: "14 days",
-        optionC: "5 days",
-        optionD: "7 days",
-        correctOption: "optionD"
+        question: " A runaway princess attracts dragon people",
+        optionA: "Yona of the dawn",
+        optionB: "orange",
+        optionC: "squid girl",
+        optionD: " angel beats",
+        correctOption: "optionA"
     },
 
     {
-        question: "How many players are allowed on a soccer pitch ?",
-        optionA: "10 players",
-        optionB: "11 players",
-        optionC: "9 players",
-        optionD: "12 players",
+        question: "It's totally normal for ninjas to wear brigtly colored clothin",
+        optionA: "orange",
+        optionB: "Naruto",
+        optionC: "RElife",
+        optionD: "Pokemon",
         correctOption: "optionB"
     },
 
     {
-        question: "Who was the first President of USA ?",
-        optionA: "Donald Trump",
-        optionB: "Barack Obama",
-        optionC: "Abraham Lincoln",
-        optionD: "George Washington",
+        question: "A dangerous octopus is appeasedwith a teaching position",
+        optionA: "Yona of the dawn",
+        optionB: "Death Parade",
+        optionC: "Yurikima Arashi",
+        optionD: "Assassination Classroom",
         correctOption: "optionD"
     },
 
     {
-        question: "30 days has ______ ?",
-        optionA: "January",
-        optionB: "December",
-        optionC: "June",
-        optionD: "August",
+        question: "Two geniuses play games with eachother while a third dude has an apple addiction",
+        optionA: "No game no life",
+        optionB: "Code geass",
+        optionC: "Death Note",
+        optionD: "Soul eater",
         correctOption: "optionC"
     },
 
     {
-        question: "How manay hours can be found in a day ?",
-        optionA: "30 hours",
-        optionB: "38 hours",
-        optionC: "48 hours",
-        optionD: "24 hours",
+        question: "School but everybody has powers, nobody knows what one character looks like",
+        optionA: "Soul eater",
+        optionB: "Dangonronpa",
+        optionC: "Demon School",
+        optionD: "My hero academia",
         correctOption: "optionD"
     },
 
     {
-        question: "Which is the longest river in the world ?",
-        optionA: "River Nile",
-        optionB: "Long River",
-        optionC: "River Niger",
-        optionD: "Lake Chad",
+        question: "You will drown if you eat fruit",
+        optionA: "One piece",
+        optionB: "Bakuman",
+        optionC: "Fullmetal Alchemist",
+        optionD: "Naruto",
         correctOption: "optionA"
     },
 
     {
-        question: "_____ is the hottest Continent on Earth ?",
-        optionA: "Oceania",
-        optionB: "Antarctica",
-        optionC: "Africa",
-        optionD: "North America",
+        question: "Bully becomes bullied",
+        optionA: "One punch man",
+        optionB: "Gundam",
+        optionC: "Silent Voice",
+        optionD: "Digimon",
         correctOption: "optionC"
     },
 
     {
-        question: "Which country is the largest in the world ?",
-        optionA: "Russia",
-        optionB: "Canada",
-        optionC: "Africa",
-        optionD: "Egypt",
+        question: "Finding Daddy",
+        optionA: "Hunter X Hunter",
+        optionB: "Pokemon",
+        optionC: "Black Clover",
+        optionD: "Free",
         correctOption: "optionA"
     },
 
     {
-        question: "Which of these numbers is an odd number ?",
-        optionA: "Ten",
-        optionB: "Twelve",
-        optionC: "Eight",
-        optionD: "Eleven",
+        question: "Thick headed boy tries to save his Sister",
+        optionA: "Kill la kill",
+        optionB: "Gintama",
+        optionC: "Gundam",
+        optionD: "Demon Slayer",
         correctOption: "optionD"
     },
 
     {
-        question: `"You Can't see me" is a popular saying by`,
-        optionA: "Eminem",
-        optionB: "Bill Gates",
-        optionC: "Chris Brown",
-        optionD: "John Cena",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Where is the world tallest building located ?",
-        optionA: "Africa",
-        optionB: "California",
-        optionC: "Dubai",
-        optionD: "Italy",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "The longest river in the United Kingdom is ?",
-        optionA: "River Severn",
-        optionB: "River Mersey",
-        optionC: "River Trent",
-        optionD: "River Tweed",
+        question: "Trying to save the forest from evil",
+        optionA: "Princess Mononoke",
+        optionB: "Spirited Away",
+        optionC: "Dr.stone",
+        optionD: "Gundam",
         correctOption: "optionA"
     },
 
 
     {
-        question: "How many permanent teeth does a dog have ?",
-        optionA: "38",
-        optionB: "42",
-        optionC: "40",
-        optionD: "36",
+        question: "swap your body with a radom person and write to know each other",
+        optionA: "Silent Voice",
+        optionB: "Your Name",
+        optionC: "Gintama",
+        optionD: "Fate series",
         correctOption: "optionB"
     },
 
     {
-        question: "Which national team won the football World cup in 2018 ?",
-        optionA: "England",
-        optionB: "Brazil",
-        optionC: "Germany",
-        optionD: "France",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "Which US state was Donald Trump Born ?",
-        optionA: "New York",
-        optionB: "California",
-        optionC: "New Jersey",
-        optionD: "Los Angeles",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "How man states does Nigeria have ?",
-        optionA: "24",
-        optionB: "30",
-        optionC: "36",
-        optionD: "37",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "____ is the capital of Nigeria ?",
-        optionA: "Abuja",
-        optionB: "Lagos",
-        optionC: "Calabar",
-        optionD: "Kano",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "Los Angeles is also known as ?",
-        optionA: "Angels City",
-        optionB: "Shining city",
-        optionC: "City of Angels",
-        optionD: "Lost Angels",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "What is the capital of Germany ?",
-        optionA: "Georgia",
-        optionB: "Missouri",
-        optionC: "Oklahoma",
-        optionD: "Berlin",
-        correctOption: "optionD"
-    },
-
-    {
-        question: "How many sides does an hexagon have ?",
-        optionA: "Six",
-        optionB: "Sevene",
-        optionC: "Four",
-        optionD: "Five",
-        correctOption: "optionA"
-    },
-
-    {
-        question: "How many planets are currently in the solar system ?",
+        question: "Monkey yells really loud",
         optionA: "Eleven",
-        optionB: "Seven",
-        optionC: "Nine",
-        optionD: "Eight",
+        optionB: "Black clover",
+        optionC: "Attack on titan",
+        optionD: "DragonBallZ",
         correctOption: "optionD"
     },
-
-    {
-        question: "Which Planet is the hottest ?",
-        optionA: "Jupitar",
-        optionB: "Mercury",
-        optionC: "Earth",
-        optionD: "Venus",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "where is the smallest bone in human body located?",
-        optionA: "Toes",
-        optionB: "Ears",
-        optionC: "Fingers",
-        optionD: "Nose",
-        correctOption: "optionB"
-    },
-
-    {
-        question: "How many hearts does an Octopus have ?",
-        optionA: "One",
-        optionB: "Two",
-        optionC: "Three",
-        optionD: "Four",
-        correctOption: "optionC"
-    },
-
-    {
-        question: "How many teeth does an adult human have ?",
-        optionA: "28",
-        optionB: "30",
-        optionC: "32",
-        optionD: "36",
-        correctOption: "optionC"
-    }
 
 ]
 
 
-let shuffledQuestions = [] // array to hold shuffled selected questions out of available questions
+let shuffledQuestions = [] 
 
 function handleQuestions() { 
-    //function to shuffle and push 10 questions to shuffledQuestions array
-//app would be dealing with 10questions per session
     while (shuffledQuestions.length <= 9) {
         const random = questions[Math.floor(Math.random() * questions.length)]
         if (!shuffledQuestions.includes(random)) {
@@ -242,13 +123,11 @@ function handleQuestions() {
 }
 
 
-let questionNumber = 1 //holds the current question number
-let playerScore = 0  //holds the player score
-let wrongAttempt = 0 //amount of wrong answers picked by player
-let indexNumber = 0 //will be used in displaying next question
+let questionNumber = 1
+let playerScore = 0 
+let wrongAttempt = 0 
+let indexNumber = 0
 
-// function for displaying next question in the array to dom
-//also handles displaying players and quiz information to dom
 function NextQuestion(index) {
     handleQuestions()
     const currentQuestion = shuffledQuestions[index]
@@ -264,30 +143,26 @@ function NextQuestion(index) {
 
 
 function checkForAnswer() {
-    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
-    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
-    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    const currentQuestion = shuffledQuestions[indexNumber] 
+    const currentQuestionAnswer = currentQuestion.correctOption 
+    const options = document.getElementsByName("option"); 
     let correctOption = null
 
     options.forEach((option) => {
         if (option.value === currentQuestionAnswer) {
-            //get's correct's radio input with correct answer
             correctOption = option.labels[0].id
         }
     })
 
-    //checking to make sure a radio input has been checked or an option being chosen
     if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
         document.getElementById('option-modal').style.display = "flex"
     }
 
-    //checking if checked radio button is same as answer
     options.forEach((option) => {
         if (option.checked === true && option.value === currentQuestionAnswer) {
             document.getElementById(correctOption).style.backgroundColor = "green"
-            playerScore++ //adding to player's score
-            indexNumber++ //adding 1 to index so has to display next question..
-            //set to delay question number till when next question loads
+            playerScore++ 
+            indexNumber++ 
             setTimeout(() => {
                 questionNumber++
             }, 1000)
@@ -297,9 +172,9 @@ function checkForAnswer() {
             const wrongLabelId = option.labels[0].id
             document.getElementById(wrongLabelId).style.backgroundColor = "red"
             document.getElementById(correctOption).style.backgroundColor = "green"
-            wrongAttempt++ //adds 1 to wrong attempts 
+            wrongAttempt++ 
             indexNumber++
-            //set to delay question number till when next question loads
+            
             setTimeout(() => {
                 questionNumber++
             }, 1000)
@@ -309,24 +184,21 @@ function checkForAnswer() {
 
 
 
-//called when the next button is called
+
 function handleNextQuestion() {
-    checkForAnswer() //check if player picked right or wrong option
+    checkForAnswer() 
     unCheckRadioButtons()
-    //delays next question displaying for a second just for some effects so questions don't rush in on player
     setTimeout(() => {
         if (indexNumber <= 9) {
-//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
             NextQuestion(indexNumber)
         }
         else {
-            handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
+            handleEndGame()
         }
         resetOptionBackground()
     }, 1000);
 }
 
-//sets options background back to null after display the right/wrong colors
 function resetOptionBackground() {
     const options = document.getElementsByName("option");
     options.forEach((option) => {
@@ -334,7 +206,6 @@ function resetOptionBackground() {
     })
 }
 
-// unchecking all radio buttons for next question(can be done with map or foreach loop also)
 function unCheckRadioButtons() {
     const options = document.getElementsByName("option");
     for (let i = 0; i < options.length; i++) {
@@ -342,27 +213,24 @@ function unCheckRadioButtons() {
     }
 }
 
-// function for when all questions being answered
 function handleEndGame() {
     let remark = null
     let remarkColor = null
 
-    // condition check for player remark and remark color
     if (playerScore <= 3) {
-        remark = "Bad Grades, Keep Practicing."
+        remark = "get some recommendations dude"
         remarkColor = "red"
     }
     else if (playerScore >= 4 && playerScore < 7) {
-        remark = "Average Grades, You can do better."
+        remark = "i feel ya."
         remarkColor = "orange"
     }
     else if (playerScore >= 7) {
-        remark = "Excellent, Keep the good work going."
+        remark = "dayumm , you good!!"
         remarkColor = "green"
     }
     const playerGrade = (playerScore / 10) * 100
 
-    //data to display to score board
     document.getElementById('remarks').innerHTML = remark
     document.getElementById('remarks').style.color = remarkColor
     document.getElementById('grade-percentage').innerHTML = playerGrade
@@ -372,7 +240,7 @@ function handleEndGame() {
 
 }
 
-//closes score modal, resets game and reshuffles questions
+
 function closeScoreModal() {
     questionNumber = 1
     playerScore = 0
@@ -383,7 +251,6 @@ function closeScoreModal() {
     document.getElementById('score-modal').style.display = "none"
 }
 
-//function to close warning modal
 function closeOptionModal() {
     document.getElementById('option-modal').style.display = "none"
 }
